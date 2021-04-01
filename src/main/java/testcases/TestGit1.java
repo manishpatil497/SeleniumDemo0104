c2a9ddb2032e5bf1eb78100fe4e8a436615001d0
manishpatil497 <manishpatil497@gmail.com>
2021-04-01 15:08:38 +0530

    another guy commit1

---
+++ src/main/java/testcases/TestGit1.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class TestGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sel1");
+		System.out.println("Sel2");
+		System.out.println("Sel3");
+	}
+
+}
